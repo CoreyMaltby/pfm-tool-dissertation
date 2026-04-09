4f7d94277b72f336bd91f85d42f93c446771ada1
Corey Maltby <corey.maltby@yahoo.co.uk>
2026-04-09 09:28:46 +0100

    setup

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,11 @@
-// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { fileURLToPath } from 'url'
-import path from 'path'
-
-const __dirname = path.dirname(fileURLToPath(import.meta.url))
 
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      'read-excel-file': path.resolve(__dirname, 'node_modules/read-excel-file/bundle/read-excel-file.min.js')
-    }
-  }
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './src/test/setup.js',
+  },
 })
